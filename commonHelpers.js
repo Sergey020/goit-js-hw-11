120a2fc81d157560a2d465768ea629586833e302
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-04-07 14:17:20 +0000

    Deploying to gh-pages from @ Sergey020/goit-js-hw-11@33636acc0724c777621de591312673887a1dd53e 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,20 +1,28 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&o(l)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const i=document.querySelector(".search-form"),a=document.querySelector("ul");i.addEventListener("summit",u);function u(c){c.preventDefault(),c.target.elements.imgName.value,f().then(s=>(a.innerHtml=m(s)).catch(o=>console.log(o)))}console.log(imputEl);function f(c){const r="https://pixabay.com/api",s="43264129-0a20e73c7f6f9c0a58d1fd1ca",o=new URLSearchParams({key:s,q:dog,imege_type:photo,orientation:horizontal,safesearch:!0});return fetch(`${r}&${o}`).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}function m(c){return c.map(({webformatURL:r,largeImageURL:s,tags:o,likes:e,views:t,comments:l,downloads:n})=>`<li class="gallery-item">
-        <a class="gallery-link" href=${s}>
-            <img 
-                class="gallery-image" 
-                src=${r} 
-                alt=${o}
-            />
-            <div class="box-card">
-                <h2 class="box-title">Likes</h2>
-                <p class="box-text">${e}</p>
-                <h2 class="box-title">Views</h2>
-                <p class="box-text">${t}</p>
-                <h2 class="box-title">Comments</h2>
-                <p class="box-text">${l}</p>
-                <h2 class="box-title">Downloads</h2>
-                <p class="box-text">${n}</p>
-            </div>
-        </a>
-      </li>`).join("")}
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))l(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const o of e.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function r(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function l(t){if(t.ep)return;t.ep=!0;const e=r(t);fetch(t.href,e)}})();const h="https://pixabay.com/api/",p="43261093-4c9b6888dd775193ca00ecdb2",d=document.querySelector("form"),g=document.querySelector(".gallery");d.addEventListener("submit",y);function y(c){c.preventDefault();const r=c.target.elements.imgName.value;console.log(r);const l=new URLSearchParams({key:p,q:`${r}`,imege_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${h}?${l}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>{console.log(e.hits),g.innerHTML=t(e.hits)}).catch(e=>console.log("catch",e));function t(e){return e.map(({webformatURL:o,largeImageURL:i,tags:n,likes:a,views:u,comments:f,downloads:m})=>`<li class="gallery-item">
+          <a class="gallery-link" href=${i}>
+              <img 
+                  class="gallery-image" 
+                  src=${o} 
+                  alt=${n}
+              />
+              <ul class="box-list">
+                  <li>
+                      <h2 class="box-title">Likes</h2>
+                      <p class="box-text">${a}</p>
+                  </li>
+                  <li>
+                      <h2 class="box-title">Views</h2>
+                      <p class="box-text">${u}</p>
+                  </li>
+                  <li class="box-item">
+                      <h2 class="box-title">Comments</h2>
+                      <p class="box-text">${f}</p>
+                  </li>
+                  <li class="box-item">
+                      <h2 class="box-title">Downloads</h2>
+                      <p class="box-text">${m}</p>
+                  </li>
+              </ul>
+          </a>
+        </li>`).join("")}}
 //# sourceMappingURL=commonHelpers.js.map
