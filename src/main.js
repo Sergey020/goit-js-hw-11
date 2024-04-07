e45cf6e7b951469e1842b656e46f72913ecdab07
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-04-07 22:21:48 +0200

    im

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+import iziToast from  "izitoast" ;
+import  "izitoast/dist/css/iziToast.min.css" ;
+
+
+
+
 // https://pixabay.com/api/?key=43264129-0a20e73c7f6f9c0a58d1fd1ca&q=dog&image_type=photo&orientation=horizontal&safesearch=true
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43264129-0a20e73c7f6f9c0a58d1fd1ca';
@@ -13,9 +19,9 @@ function hendleSubmit(event) {
   const inputEl = form.elements.imgName.value;
   console.log(inputEl);
 
-  //console.log(searchImage(inputEl));
-  // .then(resp => (ulEl.innerHtml = createMarckup(resp))
-  // .catch(err => console.log(err)));
+if(inputEl === "") {
+  alert("Field must be filled !")
+}
 
   const params = new URLSearchParams({
     key: API_KEY,
@@ -34,7 +40,7 @@ function hendleSubmit(event) {
     })
     .then(data => {
       console.log(data.hits);
-      ulEl.insertAdjacentHTML("beforeend", createMarkup(data.hits))
+      ulEl.insertAdjacentHTML('beforeend', createMarkup(data.hits));
     })
     .catch(error => console.log('catch', error));
 
@@ -50,32 +56,31 @@ function hendleSubmit(event) {
           comments,
           downloads,
         }) => `<li class="gallery-item">
-          <a class="gallery-link" href=${largeImageURL}>
-              <img 
-                  class="gallery-image" 
-                  src=${webformatURL} 
-                  alt=${tags}
-              />
-              <ul class="box-list">
-                  <li>
-                      <h2 class="box-title">Likes</h2>
-                      <p class="box-text">${likes}</p>
-                  </li>
-                  <li>
-                      <h2 class="box-title">Views</h2>
-                      <p class="box-text">${views}</p>
-                  </li>
-                  <li class="box-item">
-                      <h2 class="box-title">Comments</h2>
-                      <p class="box-text">${comments}</p>
-                  </li>
-                  <li class="box-item">
-                      <h2 class="box-title">Downloads</h2>
-                      <p class="box-text">${downloads}</p>
-                  </li>
-              </ul>
-          </a>
-        </li>`
+<a class="gallery-link" href=${largeImageURL}>
+   <img 
+   class="gallery-image" 
+  src="${webformatURL}" 
+  alt="${tags}"/>
+ <ul class="box-list">
+   <li>
+    <h2 class="box-title">Likes</h2>
+     <p class="box-text">${likes}</p>
+     </li>
+    <li>
+    <h2 class="box-title">Views</h2>
+    <p class="box-text">${views}</p>
+    </li>
+    <li class="box-item">
+    <h2 class="box-title">Comments</h2>
+    <p class="box-text">${comments}</p>
+    </li>
+    <li class="box-item">
+    <h2 class="box-title">Downloads</h2>
+    <p class="box-text">${downloads}</p>
+    </li>
+    </ul>
+    </a>
+    </li>`
       )
       .join('');
   }
