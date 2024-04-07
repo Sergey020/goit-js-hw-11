33636acc0724c777621de591312673887a1dd53e
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-04-07 17:17:11 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,71 +1,115 @@
-const searchEl = document.querySelector('.search-form');
-const ulEl = document.querySelector('ul');
+// https://pixabay.com/api/?key=43264129-0a20e73c7f6f9c0a58d1fd1ca&q=dog&image_type=photo&orientation=horizontal&safesearch=true
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '43261093-4c9b6888dd775193ca00ecdb2';
 
-searchEl.addEventListener('summit', hendleSubmit);
+const searchEl = document.querySelector('form');
+const ulEl = document.querySelector('.gallery');
+
+searchEl.addEventListener('submit', hendleSubmit);
 
 function hendleSubmit(event) {
   event.preventDefault();
   const form = event.target;
   const inputEl = form.elements.imgName.value;
+  console.log(inputEl);
 
-  searchImage(inputEl).then(resp =>
-    (ulEl.innerHtml = createMarckup(resp)).catch(err => console.log(err))
-  );
-}
-console.log(imputEl);
+  //console.log(searchImage(inputEl));
+  // .then(resp => (ulEl.innerHtml = createMarckup(resp))
+  // .catch(err => console.log(err)));
 
-
-function searchImage(inputEl) {
-  // https://pixabay.com/api/?key=43264129-0a20e73c7f6f9c0a58d1fd1ca&q=dog&image_type=photo&orientation=horizontal&safesearch=true
-  const BASE_URL = 'https://pixabay.com/api';
-  const API_KEY = '43264129-0a20e73c7f6f9c0a58d1fd1ca';
   const params = new URLSearchParams({
     key: API_KEY,
-    q: dog,
-    imege_type: photo,
-    orientation: horizontal,
+    q: `${inputEl}`,
+    imege_type: 'photo',
+    orientation: 'horizontal',
     safesearch: true,
   });
 
-  return fetch(`${BASE_URL}&${params}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
-}
+  return fetch(`${BASE_URL}?${params}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.hits);
+      ulEl.innerHTML = createMarckup(data.hits)
+    })
+    .catch(error => console.log('catch', error));
 
-function createMarckup(arrImg) {
-  return arrImg
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `<li class="gallery-item">
-        <a class="gallery-link" href=${largeImageURL}>
-            <img 
-                class="gallery-image" 
-                src=${webformatURL} 
-                alt=${tags}
-            />
-            <div class="box-card">
-                <h2 class="box-title">Likes</h2>
-                <p class="box-text">${likes}</p>
-                <h2 class="box-title">Views</h2>
-                <p class="box-text">${views}</p>
-                <h2 class="box-title">Comments</h2>
-                <p class="box-text">${comments}</p>
-                <h2 class="box-title">Downloads</h2>
-                <p class="box-text">${downloads}</p>
-            </div>
-        </a>
-      </li>`
-    )
-    .join('');
+  function createMarckup(arrImg) {
+    return arrImg
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<li class="gallery-item">
+          <a class="gallery-link" href=${largeImageURL}>
+              <img 
+                  class="gallery-image" 
+                  src=${webformatURL} 
+                  alt=${tags}
+              />
+              <ul class="box-list">
+                  <li>
+                      <h2 class="box-title">Likes</h2>
+                      <p class="box-text">${likes}</p>
+                  </li>
+                  <li>
+                      <h2 class="box-title">Views</h2>
+                      <p class="box-text">${views}</p>
+                  </li>
+                  <li class="box-item">
+                      <h2 class="box-title">Comments</h2>
+                      <p class="box-text">${comments}</p>
+                  </li>
+                  <li class="box-item">
+                      <h2 class="box-title">Downloads</h2>
+                      <p class="box-text">${downloads}</p>
+                  </li>
+              </ul>
+          </a>
+        </li>`
+      )
+      .join('');
+  }
+  // function createMarckup(arrImg) {
+  //   return arrImg
+  //     .map(
+  //       ({
+  //         webformatURL,
+  //         largeImageURL,
+  //         tags,
+  //         likes,
+  //         views,
+  //         comments,
+  //         downloads,
+  //       }) => `<li class="gallery-item">
+  //       <a class="gallery-link" href=${largeImageURL}>
+  //           <img
+  //               class="gallery-image"
+  //               src=${webformatURL}
+  //               alt=${tags}
+  //           />
+  //           <div class="box-card">
+  //               <h2 class="box-title">Likes</h2>
+  //               <p class="box-text">${likes}</p>
+  //               <h2 class="box-title">Views</h2>
+  //               <p class="box-text">${views}</p>
+  //               <h2 class="box-title">Comments</h2>
+  //               <p class="box-text">${comments}</p>
+  //               <h2 class="box-title">Downloads</h2>
+  //               <p class="box-text">${downloads}</p>
+  //           </div>
+  //       </a>
+  //     </li>`
+  //     )
+  //     .join('');
+  // }
 }
